70f0af886df7b8a846b596433d39f9bb9c0477d6
Felipe Moreira <fmoreirasouza701@gmail.com>
2024-10-17 21:53:42 -0400

    Initial Commit

---
+++ PetZoneTests/PetZoneTests.swift
@@ -0,0 +1,17 @@
+//
+//  PetZoneTests.swift
+//  PetZoneTests
+//
+//  Created by user264582 on 10/16/24.
+//
+
+import Testing
+@testable import PetZone
+
+struct PetZoneTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
